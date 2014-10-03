b0db7eaaaba6b3495ac92d00fbb6041f746209c0
capellil <capelli.ludovic@gmail.com>
2014-10-04 00:29:28 +0100

    More rigourous way of programming. No need to free the memory as at the first fail from this file the programme stops.

--- run_number_generator_and_console.c
+++ run_number_generator_and_console.c
@@ -3,38 +3,52 @@
 #include "console_process_int.h"
 #include "processes_list.h"
 
+#include <stdio.h> // printf
 #include <stdlib.h> // NULL | EXIT_SUCCESS
 
 int main(int argc, char* argv[])
 {
-	// Creation of the channel
-	struct one_2_one_channel_t* one_2_one_channel = create_one_2_one_channel();
-
-	// Creation of the process generating numbers
-	struct number_generator_process* number_generator_process = create_number_generator_process(0, 200, 1, one_2_one_channel->channel_output_end);
-
-	// Creation of the process displaying what it receives
-	char prefix[] = "Received from number generator process : \"";
-	char suffix[] = "\".";
-	struct console_process_int* console_process_int = create_console_process_int(prefix, suffix, one_2_one_channel->channel_input_end);
-	
-	struct processes_list_t* processes_list = create_processes_list(2);
-	if(processes_list != NULL)
+	// Creation of the channel between the number generator and the console
+	struct one_2_one_channel_t* number_to_console = create_one_2_one_channel();
+	if(number_to_console)
 	{
-		insert_processes_list(processes_list, number_generator_process, number_generator_process->run);
-		insert_processes_list(processes_list, console_process_int, console_process_int->run);
+		// Creation of the process generating numbers
+		struct number_generator_process* number_generator_process = create_number_generator_process(0, 200, 1, number_to_console->channel_output_end);
+		if(number_generator_process)
+		{
+			// Creation of the process displaying what it receives
+			char prefix[] = "Received from number generator process : \"";
+			char suffix[] = "\".";
+			struct console_process_int* console_process_int = create_console_process_int(prefix, suffix, number_to_console->channel_input_end);
+			if(console_process_int)
+			{
+				struct processes_list_t* processes_list = create_processes_list(2);
+				if(processes_list != NULL)
+				{
+					insert_processes_list(processes_list, number_generator_process, number_generator_process->run);
+					insert_processes_list(processes_list, console_process_int, console_process_int->run);
 	
-		run_in_parallel(processes_list);
+					run_in_parallel(processes_list);
+				}
+				else
+				{
+					printf("FATAL ERROR : Impossible to create the processes list.\n");
+				}
+			}
+			else
+			{
+				printf("FATAL ERROR : Impossible to create the console process int.\n");
+			}
+		}
+		else
+		{
+			printf("FATAL ERROR : Impossible to create the number generator process.\n");
+		}
 	}
 	else
 	{
-		free(number_generator_process);
-		number_generator_process = NULL;
-		free(console_process_int);
-		console_process_int = NULL;
+		printf("FATAL ERROR : Impossible to create the channel between the number generator output to the console input.\n");
 	}
-
-	free(one_2_one_channel);
 	
 	return EXIT_SUCCESS;
 }
