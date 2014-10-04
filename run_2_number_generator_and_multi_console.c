05009770162c24c93363d73a90c4ac73157be45e
capellil <capelli.ludovic@gmail.com>
2014-10-04 01:08:21 +0100

    More rigourous way of programming. No need to free the memory as at the first fail from this file the programme stops & return value corrected.

--- run_2_number_generator_and_multi_console.c
+++ run_2_number_generator_and_multi_console.c
@@ -8,61 +8,106 @@
 
 int main(int argc, char* argv[])
 {
-	// Creation of the channel
-	struct one_2_one_channel_t* number_1_to_delayer = create_one_2_one_channel();
-	struct one_2_one_channel_t* delayer_to_console = create_one_2_one_channel();
-	struct one_2_one_channel_t* number_2_to_console = create_one_2_one_channel();
-
-	// Creation of the process generating numbers
-	struct number_generator_process* number_generator_process_1 = create_number_generator_process(0, 200, 1, number_1_to_delayer->channel_output_end);
+	int result = EXIT_FAILURE;
 	
-	struct delayer_process* delayer_process = create_delayer_process(50, number_1_to_delayer->channel_input_end, delayer_to_console->channel_output_end);
-	struct channel_end_generic_t* channel_end_generic_1 = (struct channel_end_generic_t*)malloc(sizeof(struct channel_end_generic_t));
-	channel_end_generic_1->type = CHANNEL_INPUT_TYPE;
-	channel_end_generic_1->channel_end = (void*)(delayer_to_console->channel_input_end);
-	
-	struct number_generator_process* number_generator_process_2 = create_number_generator_process(200, 400, 1, number_2_to_console->channel_output_end);
-	struct channel_end_generic_t* channel_end_generic_2 = (struct channel_end_generic_t*)malloc(sizeof(struct channel_end_generic_t));
-	channel_end_generic_2->type = CHANNEL_INPUT_TYPE;
-	channel_end_generic_2->channel_end = (void*)(number_2_to_console->channel_input_end);
-	
-	// Creation of the process displaying what it receives
-	char prefix[] = "Received from number generator process : \"";
-	char suffix[] = "\".";
-	struct multi_console_process* multi_console_process = create_multi_console_process(prefix, suffix, 2);
-	add_to_alternative(channel_end_generic_1, multi_console_process->alternative);
-	add_to_alternative(channel_end_generic_2, multi_console_process->alternative);
-	
-	struct processes_list_t* processes_list = create_processes_list(3);
-	if(processes_list != NULL)
+	// Creation of the channel between the first number generator to the delay process
+	struct one_2_one_channel_t* number_1_to_delay = create_one_2_one_channel();
+	if(number_1_to_delay)
 	{
-		insert_processes_list(processes_list, number_generator_process_1, number_generator_process_1->run);
-		insert_processes_list(processes_list, delayer_process, delayer_process->run);
-		insert_processes_list(processes_list, number_generator_process_2, number_generator_process_2->run);
-		insert_processes_list(processes_list, multi_console_process, multi_console_process->run);
+		// Creation of the channel between the delay process and the multi console process
+		struct one_2_one_channel_t* delay_to_console = create_one_2_one_channel();
+		if(delay_to_console)
+		{
+			// Creation of the channel between the second number generator and the multi console process
+			struct one_2_one_channel_t* number_2_to_console = create_one_2_one_channel();
+			if(number_2_to_console)
+			{
+				// Creation of the first number generator
+				struct number_generator_process* number_generator_process_1 = create_number_generator_process(0, 200, 1, number_1_to_delay->channel_output_end);
+				if(number_generator_process_1)
+				{
+					// Creation of the delay process
+					struct delay_process* delay_process = create_delay_process(50, number_1_to_delay->channel_input_end, delay_to_console->channel_output_end);
+					if(delay_process)
+					{
+						// Creation of the generic channel
+						struct channel_end_generic_t* channel_end_generic_1 = (struct channel_end_generic_t*)malloc(sizeof(struct channel_end_generic_t));
+						if(channel_end_generic_1)
+						{
+							channel_end_generic_1->type = CHANNEL_INPUT_TYPE;
+							channel_end_generic_1->channel_end = (void*)(delay_to_console->channel_input_end);
+							
+							// Creation of the second number generator process
+							struct number_generator_process* number_generator_process_2 = create_number_generator_process(200, 400, 1, number_2_to_console->channel_output_end);
+							if(number_generator_process_2)
+							{
+								struct channel_end_generic_t* channel_end_generic_2 = (struct channel_end_generic_t*)malloc(sizeof(struct channel_end_generic_t));
+								channel_end_generic_2->type = CHANNEL_INPUT_TYPE;
+								channel_end_generic_2->channel_end = (void*)(number_2_to_console->channel_input_end);
 	
-		run_in_parallel(processes_list);
+								// Creation of the process displaying what it receives
+								char prefix[] = "Received from number generator process : \"";
+								char suffix[] = "\".";
+								struct multi_console_process* multi_console_process = create_multi_console_process(prefix, suffix, 2);
+								if(multi_console_process)
+								{
+									add_to_alternative(channel_end_generic_1, multi_console_process->alternative);
+									add_to_alternative(channel_end_generic_2, multi_console_process->alternative);
+	
+									struct processes_list_t* processes_list = create_processes_list(3);
+									if(processes_list != NULL)
+									{
+										insert_processes_list(processes_list, number_generator_process_1, number_generator_process_1->run);
+										insert_processes_list(processes_list, delay_process, delay_process->run);
+										insert_processes_list(processes_list, number_generator_process_2, number_generator_process_2->run);
+										insert_processes_list(processes_list, multi_console_process, multi_console_process->run);
+	
+										run_in_parallel(processes_list);
+									}
+									else
+									{
+										printf("FATAL ERROR : Impossible to create the processes list.\n");
+									}
+								}
+								else
+								{
+									printf("FATAL ERROR : Impossible to create the multi console process.\n");
+								}
+							}
+							else
+							{
+								printf("FATAL ERROR : Impossible to create the second number generator process.\n");
+							}
+						}
+						else
+						{
+							printf("FATAL ERROR : Impossible to create the generic channel.\n");
+						}
+					}
+					else
+					{
+						printf("FATAL ERROR : Impossible to create the delay process.\n");
+					}
+				}
+				else
+				{
+					printf("FATAL ERROR : Impossible to create the first number generator process.\n");
+				}
+			}
+			else
+			{
+				printf("FATAL ERROR : Impossible to create the channel between the second number generator output and the multi console input.\n");
+			}
+		}
+		else
+		{
+			printf("FATAL ERROR : Impossible to create the channel between the delay output and the multi console input.\n");
+		}
 	}
 	else
 	{
-		free(number_generator_process_1);
-		number_generator_process_1 = NULL;
-		
-		free(number_generator_process_2);
-		number_generator_process_2 = NULL;
-		
-		free(multi_console_process);
-		multi_console_process = NULL;
+		printf("FATAL ERROR : Impossible to create the channel between the first number generator output and the delay input.\n");
 	}
-
-	free(number_1_to_delayer); 
-	number_1_to_delayer = NULL;
 	
-	free(delayer_to_console);
-	delayer_to_console = NULL;
-	
-	free(number_2_to_console);
-	number_2_to_console = NULL;
-	
-	return EXIT_SUCCESS;
+	return result;
 }
