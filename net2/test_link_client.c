e29b1d0167e4b0152fd503925133b3e33fa319b9
Capellil <capelli.ludovic@gmail.com>
2015-01-02 01:52:55 +0100

    Input the IP address under string format => Easier to use.

--- net2/test_link_client.c
+++ net2/test_link_client.c
@@ -1,19 +1,39 @@
-#include <stdio.h>
+#include <stdio.h> // printf
+#include <stdlib.h> // EXIT_SUCCESS | EXIT_FAILURE
+#include <netinet/in.h> // sockaddr_in | IPPROTO_TCP | IPPROTO_UDP
 
 #include "net2_link.h"
 
 int main(int argc, char* argv[])
 {
-    struct net2_link_t link;
-
-    if(!net2_create_client_link(&link, 3002, 3232235851, 3001))
+    unsigned int result = EXIT_FAILURE;
+    
+    if(argc == 2)
     {
-        printf("Connection succeeded.\n");
+        struct sockaddr_in ip_address;
+        if(inet_pton(AF_INET, argv[1], &ip_address) > 0)
+        {
+            struct net2_link_t link;
+    
+            if(net2_create_client_link(&link, 3002, ip_address.sin_addr.s_addr, 3001) >= 0)
+            {
+                printf("Connection succeeded.\n");
+                result = EXIT_SUCCESS;
+            }
+            else
+            {
+                perror("Connection");
+            }
+        }
+        else
+        {
+            perror("IP address conversion from string to number");
+        }
     }
     else
     {
-        perror("Connection");
+        printf("HOW TO USE THIS PROGRAM : \"./%s a.b.c.d\" with \"a.b.c.d\" the IPv4 address to use.\n", argv[0]);
     }
 
-    return 0;
+    return result;
 }
