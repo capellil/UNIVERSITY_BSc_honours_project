7a21485d2334c0a95a65736658c4c8639b8db5df
Capellil <capelli.ludovic@gmail.com>
2015-02-10 16:13:02 +0000

    Structures containing links for linked list purpose are now located in this file.

--- net2/net2_link.h
+++ net2/net2_link.h
@@ -8,8 +8,35 @@
 #ifndef NET2_LINK_INCLUDED
 #define NET2_LINK_INCLUDED
 
+#include <pthread.h> // pthread_t
+
 #include "net2_socket.h"
 #include "net2_protocol.h"
+
+/**
+ * @brief Incomplete declaration to avoid errors with the mutual inclusion with net2_protocol.h
+ **/
+struct net2_message_t;
+
+/**
+ * @brief Contains the link being run as well as its thread.
+ **/
+struct net2_link_thread_linked_element_t
+{
+    pthread_t* _my_thread;
+    struct net2_link_t* _my_link;
+    struct net2_link_thread_linked_element_t* _next_link;
+};
+
+/**
+ * @class net2_link_linked_element_t
+ * @brief A linked element containing a net2 link, forming a linked list when chained.
+ **/
+struct net2_link_linked_element_t
+{
+    struct net2_link_linked_element_t* _next_link; ///< A pointer on the next net2 link
+    struct net2_link_t* _my_link; ///< A pointer on the contained net2 link
+};
 
 /**
  * @class net2_link_rx_t
@@ -27,6 +54,19 @@ struct net2_link_rx_t
 struct net2_link_tx_t
 {
     int (*write) (struct net2_socket_t*, void* data, unsigned int data_length); ///< The function to write to the link.
+};
+
+/**
+ * @class net2_link_t
+ * @brief Contains the two links to send and receive data as well as the two function to deal with both.
+ **/
+struct net2_link_t
+{
+    struct net2_socket_t* _net2_socket; ///< The net2 socket to communicate with
+    struct net2_link_rx_t _net2_link_rx; ///< The link RX
+    struct net2_link_tx_t _net2_link_tx; ///< The link TX
+    int (*read) (struct net2_link_t* net2_link_rx, void* data, unsigned int data_length); ///< The function to read from the link
+    int (*write) (struct net2_link_t* net2_link_tx, void* data, unsigned int data_length); ///< The function to write to the link
 };
 
 /**
@@ -54,19 +94,6 @@ int net2_link_rx_read(struct net2_socket_t* net2_socket, void* data, unsigned in
            </ul>
  **/
 int net2_link_tx_write(struct net2_socket_t* net2_socket, void* data, unsigned int data_length);
-
-/**
- * @class net2_link_t
- * @brief Contains the two links to send and receive data as well as the two function to deal with both.
- **/
-struct net2_link_t
-{
-    struct net2_socket_t* _net2_socket; ///< The net2 socket to communicate with
-    struct net2_link_rx_t _net2_link_rx; ///< The link RX
-    struct net2_link_tx_t _net2_link_tx; ///< The link TX
-    int (*read) (struct net2_link_t* net2_link_rx, void* data, unsigned int data_length); ///< The function to read from the link
-    int (*write) (struct net2_link_t* net2_link_tx, void* data, unsigned int data_length); ///< The function to write to the link
-};
 
 /**
  * @brief Reads from a link.
@@ -159,5 +186,27 @@ int net2_link_compare_to_link(struct net2_link_t* net2_link_a, struct net2_link_
  * @pre net2_link_to_run != NULL
  **/
 void* net2_link_run(void* net2_link_to_run);
+
+/**
+ * @brief Initialises the given linked element with the given link. Also sets the next link pointer to NULL;
+ * @param link The link to assign to the given linked element.
+ * @param element The linked element to initialise.
+ **/
+void net2_link_init_linked_element(struct net2_link_t* link, struct net2_link_linked_element_t* element);
+
+/**
+ * @brief Appends the given link into the given linked list of links. If the link is already is the current_list, nothing is done.
+ * @param current_list The link linked list to be modified.
+ * @param link The new element to append to the given linked list.
+ * @return <ul>
+               <li>SUCCESS : 0
+               <li>FAILURE : -1
+           </ul>
+ * @pre <ul>
+            <li>current_list != NULL
+            <li>new_element != NULL
+        </ul>
+ **/
+int net2_link_append_to_linked_element(struct net2_link_t* link, struct net2_link_linked_element_t** current_list);
 
 #endif // NET2_LINK_INCLUDED INCLUDED
