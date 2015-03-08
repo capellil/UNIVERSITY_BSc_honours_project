dd705e20f5ee41c5a76e10903812dc104d920698
Capellil <capelli.ludovic@gmail.com>
2015-03-08 23:11:13 +0000

    Doxygen comments updated.

--- tests/test_link_manager_client.c
+++ tests/test_link_manager_client.c
@@ -1,7 +1,7 @@
 /**
  * @file test_link_manager_client.c
  * @author Capelli, Ludovic
- * @version 2.0
+ * @version 1.0
  * @date 22/01/2015
  **/
  
@@ -13,26 +13,32 @@
 /**
  * @brief Creates a link to the server. Then it get the instance of the link manager and performs verifications to ensure the link manager functions are operational.
  *
- * Argument 0 : the name of the application. (hidden)
- * Argument 1 : the server IP address.
- * Argument 2 : the server port.
+ * <ul>
+ *     <li>Argument 0 : the name of the application. (hidden)
+ *     <li>Argument 1 : the server IP address.
+ *     <li>Argument 2 : the server port.
+ * </ul>
+ * @return <ul>
+ *             <li>Success : EXIT_SUCCESS
+ *             <li>Failure : EXIT_FAILURE
+ *         </ul>
  **/
 int main(int argc, char* argv[])
 {
     int programme_return = EXIT_SUCCESS;
-    printf("Checks the number of parameters. Is there the right number of parameters...");
     
+    printf("Checks the number of parameters. Is there the right number of parameters...");
     if(argc == 3)
     {
         printf("yes.\n");
         struct sockaddr_in ip_address;
-        printf("Checking the first parameter given : is \"%s\" a valid IPv4 address...", argv[1]);
         
+        printf("Checking the first parameter given : is \"%s\" a valid IPv4 address...", argv[1]);
         if(inet_pton(AF_INET, argv[1], &ip_address) > 0)
         {
             printf("yes.\n");
-            printf("Checking the port number : is %d a valid port number...", atoi(argv[2]));
             
+            printf("Checking the port number : is %d a valid port number...", atoi(argv[2]));
             if(atoi(argv[2]) >= 0 && atoi(argv[2]) < 65536)
             {
                 printf("yes.\n");
@@ -42,36 +48,36 @@ int main(int argc, char* argv[])
                 {
                     printf("yes.\n");
                     bool found = false;
-                    printf("Calls the link manager method to check if an address and port are already used by a link. Did the function succeed...");
                     
+                    printf("Calls the link manager method to check if an address and port are already used by a link. Did the function succeed...");
                     if(!net2_link_manager_check_address_and_port(ip_address.sin_addr.s_addr, atoi(argv[2]), &found))
                     {
                         printf("yes.\n");                
                         struct net2_socket_t socket;
-                        printf("Checks if an occurence has been found. Is this address and port available so far...");
                         
+                        printf("Checks if an occurence has been found. Is this address and port available so far...");
                         if(!found)
                         {
                             printf("yes.\n");
-                            printf("Checks the socket creation. Did the socket creation succeed...");
                             
+                            printf("Checks the socket creation. Did the socket creation succeed...");
                             if(net2_socket_create(&socket) >= 0)
                             {
                                 printf("yes.\n");
-                                printf("Checks the socket connection. Did the socket connection succeed...");
                                 
+                                printf("Checks the socket connection. Did the socket connection succeed...");
                                 if(!net2_socket_connect(&socket, ip_address.sin_addr.s_addr, atoi(argv[2])))
                                 {
                                     printf("yes.\n");
                                     struct net2_link_t link;
                                     net2_link_create(&link, &socket);
-                                    printf("Calls the link manager method to check if an address and port are already used by a link. Did the function succeed...");
                                     
+                                    printf("Calls the link manager method to check if an address and port are already used by a link. Did the function succeed...");
                                     if(!net2_link_manager_check_address_and_port(ip_address.sin_addr.s_addr, atoi(argv[2]), &found))
                                     {
-                                        printf("yes.\n");                
+                                        printf("yes.\n");      
+                                                  
                                         printf("Checks if an occurence has been found. Is this address and port busy...");
-                                        
                                         if(!found)
                                         {
                                             printf("yes.\n");
