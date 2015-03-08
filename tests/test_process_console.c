6c4b6ccb1dba0d4ad0a3fd23e5f9c409911f52b6
Capellil <capelli.ludovic@gmail.com>
2015-03-08 23:15:37 +0000

    Doxygen comments updated. Missing else statements added.

--- tests/test_process_console.c
+++ tests/test_process_console.c
@@ -1,9 +1,23 @@
+/**
+ * @file test_process_console.c
+ * @author Capelli, Ludovic
+ * @version 1.0
+ * @date 08/02/2015
+ **/
+ 
 #include <stdio.h> // printf
 #include <stdlib.h> // EXIT_SUCCESS
 
 #include "../processes/process_console.h"
 #include "../net2/net2_node.h"
 
+/**
+ * @brief Creates a console process and connects it to the copy process.
+ * @return <ul>
+ *             <li>Success : EXIT_SUCCESS
+ *             <li>Failure : EXIT_FAILURE
+ *         </ul>
+ **/
 int main(int argc, char* argv[])
 {   
     int result = EXIT_SUCCESS;
@@ -11,11 +25,16 @@ int main(int argc, char* argv[])
     unsigned short my_port = 3003;
     unsigned int my_vcn = 303;
     
+    printf("Did the node initialisation succeed... ");
     if(!net2_node_init(my_port))
     {
+        printf("yes.\n");
         struct net2_channel_input_t in;
+        
+        printf("Did the channel input creation succeed... ");
         if(!net2_channel_input_create(&in, my_vcn))
         {
+            printf("yes.\n");
             struct process_console_int_t console;
             process_console_int_create(&console, &in);
             
@@ -23,12 +42,15 @@ int main(int argc, char* argv[])
         }
         else
         {
+            printf("no.\n");
+            result = EXIT_FAILURE;
         }
     }
     else
     {
+        printf("no.\n");
+        result = EXIT_FAILURE;
     }
-    
 
     return result;
 }
