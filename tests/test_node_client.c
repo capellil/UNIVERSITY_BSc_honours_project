d43f7c9bc2fb632c9b4939c4ae584bd0d867ee38
Capellil <capelli.ludovic@gmail.com>
2015-03-08 23:14:11 +0000

    Doxygen comments updated. Code refactor to fit to standards regarding if / else general structure.

--- tests/test_node_client.c
+++ tests/test_node_client.c
@@ -1,3 +1,10 @@
+/**
+ * @file test_node_client.c
+ * @author Capelli, Ludovic
+ * @version 1.0
+ * @date 08/02/2015
+ **/
+
 #include <stdio.h> // printf
 #include <stdlib.h> // EXIT_SUCCESS
 #include <stdbool.h> // bool
@@ -8,46 +15,88 @@
 /**
  * @brief Inits the node on the given port and connects to the node at the given address and port.
  *
- * Argument 1 : The port to use for the local node.
- * Argument 2 : The IP address of the remote node.
- * Argument 3 : The port of the remote node.
+ * <ul>
+ *     <li>Argument 1 : The port to use for the local node.
+ *     <li>Argument 2 : The IP address of the remote node.
+ *     <li>Argument 3 : The port of the remote node.
+ * </ul>
+ * @return <ul>
+ *             <li>Success : EXIT_SUCCESS
+ *             <li>Failure : EXIT_FAILURE
+ *         </ul>
  **/
 int main(int argc, char* argv[])
 {
-    if(!net2_node_init(atoi(argv[1])))
-    {
-        printf("Node init ok.\n");
-        struct net2_link_t* my_link = NULL;
+    int result = EXIT_SUCCESS;
     
-        struct in_addr ip_address;
-        printf("Checking the first parameter given : is \"%s\" a valid IPv4 address...", argv[2]);
-        if(inet_pton(AF_INET, argv[2], &ip_address) > 0)
+    printf("Is there the right number of parameters... ");
+    if(argc == 4)
+    {
+        printf("yes.\n");
+        
+        printf("Did the node initialisation succeed... ");
+        if(!net2_node_init(atoi(argv[1])))
         {
-            printf("yes.\n");                        
-            if(!net2_node_connect(&my_link, htonl(ip_address.s_addr), atoi(argv[3])))
+            printf("yes.\n");
+            struct net2_link_t* my_link = NULL;
+            struct in_addr ip_address;
+            
+            printf("Checking the first parameter given : is \"%s\" a valid IPv4 address...", argv[2]);
+            if(inet_pton(AF_INET, argv[2], &ip_address) > 0)
             {
-                printf("Connection ok.\n");
-                bool found = false;
-                if(!net2_link_manager_check_address_and_port(htonl(ip_address.s_addr), atoi(argv[3]), &found))
+                printf("yes.\n");   
+                 
+                printf("Did the node connection succeed... ");                    
+                if(!net2_node_connect(&my_link, htonl(ip_address.s_addr), atoi(argv[3])))
                 {
-                    if(found)
-                        printf("A link to the given node is already established.\n");
+                    printf("yes.\n");
+                    bool found = false;
+                    
+                    printf("Did the checking of the address and port in the link manager succeed... ");
+                    if(!net2_link_manager_check_address_and_port(htonl(ip_address.s_addr), atoi(argv[3]), &found))
+                    {
+                        printf("yes.\n");
+                        
+                        printf("Has the link been found... ");
+                        if(found)
+                        {
+                            printf("yes\n");
+                        }
+                        else
+                        {
+                            printf("no.\n");
+                            result = EXIT_FAILURE;
+                        }
+                    }
+                    else
+                    {
+                        printf("no.\n");
+                        result = EXIT_FAILURE;
+                    }
+                }
+                else
+                {
+                    printf("no.\n");
+                    result = EXIT_FAILURE;
                 }
             }
             else
             {
-                printf("Connection failed.\n");
+                printf("no.\n");
+                result = EXIT_FAILURE;
             }
         }
         else
         {
             printf("no.\n");
+            result = EXIT_FAILURE;
         }
     }
     else
     {
-        printf("Node init failed.\n");
+        printf("no.\n");
+        result = EXIT_FAILURE;
     }
     
-    return EXIT_SUCCESS;
+    return result;
 }
