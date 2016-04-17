041aeb8e6ac4fba7fe647bbccf00b08f37f9cc17
Ludovic Capelli <capelli.ludovic@gmail.com>
2016-04-17 22:13:42 +0100

    Doxygen documentation included the modification on the last commit.

--- html/search/classes_1.js
+++ html/search/classes_1.js
@@ -12,8 +12,6 @@ var searchData=
   ['net2_5flink_5ft',['net2_link_t',['../structnet2__link__t.html',1,'']]],
   ['net2_5flink_5fthread_5flinked_5felement_5ft',['net2_link_thread_linked_element_t',['../structnet2__link__thread__linked__element__t.html',1,'']]],
   ['net2_5flink_5ftx_5ft',['net2_link_tx_t',['../structnet2__link__tx__t.html',1,'']]],
-  ['net2_5fmessage_5flinked_5felement_5ft',['net2_message_linked_element_t',['../structnet2__message__linked__element__t.html',1,'']]],
-  ['net2_5fmessage_5ft',['net2_message_t',['../structnet2__message__t.html',1,'']]],
   ['net2_5fnode_5fid_5ft',['net2_node_id_t',['../structnet2__node__id__t.html',1,'']]],
   ['net2_5fnode_5ft',['net2_node_t',['../structnet2__node__t.html',1,'']]],
   ['net2_5fsocket_5ft',['net2_socket_t',['../structnet2__socket__t.html',1,'']]]
